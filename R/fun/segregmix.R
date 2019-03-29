f743fb102303be89ac8af0921ae8c766a6cdadc6
Sonia Sánchez <ssanchez@azti.es>
2019-03-29 23:46:28 +0100

    New SR: segregmix

--- R/fun/segregmix.R
+++ R/fun/segregmix.R
@@ -14,17 +14,25 @@
 #
 # Distributed under the terms of the GNU GPLv3
 
+# rec is calculated as:
+# - a * SSB       , if     SSB <= b
+# - a * b         , if b < SSB <= B
+# - A * B * uncAdd, if B < SSB
+# 
+# where uncAdd corresponds to the uncertainty for SSB > B,
+# with uncAdd = @uncertainty ^ ((sd of SR residuals for SSB <= B) / (sd of SR residuals for SSB <= B) - 1)
+
 segregmix <- function () 
 {
-  logl <- function(a, b, A, B, rec, ssb) {
+  logl <- function(a, b, A, B, rec, ssb, uncAdd) {
     loglAR1(log(rec), FLQuant(log(ifelse( c(ssb) <= b, a * c(ssb), 
                                           ifelse( c(ssb) <= B, a * b, 
-                                                  A * B))), dimnames = dimnames(ssb)))
+                                                  A * B * uncAdd))), dimnames = dimnames(ssb)))
   }
   
   model <- rec ~ FLQuant(ifelse( c(ssb) <= b, a * c(ssb), 
                                  ifelse( c(ssb) <= B, a * b, 
-                                         A * B)), dimnames = dimnames(ssb))
+                                         A * B * uncAdd)), dimnames = dimnames(ssb))
   
   initial <- structure(function(rec, ssb) {
     return(FLPar(a = median(c(rec)/c(ssb), na.rm = TRUE), 
